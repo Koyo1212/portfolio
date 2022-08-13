b5ba5da7a92f74d9bdb75753c76a720b74f86a6f
taro.yamamoto <koyo36987@icloud.com>
2022-08-13 17:53:14 +0900

    portfolio

---
+++ script.js
@@ -0,0 +1,19 @@
+
+ScrollReveal({ reset: false, distance: "60px", duration: 2000 });
+ScrollReveal().reveal('.header-box__logo', { delay: 200, origin: "top" });
+ScrollReveal().reveal('.header-box__nav__list', { delay: 200, origin: "top" });
+ScrollReveal().reveal('.txt-center', { delay: 200, origin: "bottom" });
+ScrollReveal().reveal('.scrool', { delay: 200, origin: "top" });
+
+ScrollReveal().reveal('.main__skill__list__image-html', { delay: 200, origin: "left" });
+
+ScrollReveal().reveal('.main__skill__list__image-css', { delay: 200, origin: "right" });
+ScrollReveal().reveal('.main__skill__list__image-js', { delay: 200, origin: "left" });
+ScrollReveal().reveal('.main__skill__list__image-git', { delay: 200, origin: "right" });
+ScrollReveal().reveal('.main__skill__list__image-figma', { delay: 200, origin: "left" });
+ScrollReveal().reveal('.main__work__list', { delay: 200, origin: "bottom" });
+
+ScrollReveal().reveal('.button', { delay: 200, origin: "right" });
+
+ScrollReveal().reveal('.inputs', { delay: 200, origin: "right" });
+ScrollReveal().reveal('.lavel', { delay: 200, origin: "left" });
