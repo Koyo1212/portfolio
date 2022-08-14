415941217ff4e0e0e9760a9f4ec3db8ea2247ff2
taro.yamamoto <koyo36987@icloud.com>
2022-08-15 02:11:00 +0900

    修正3

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ ScrollReveal().reveal('.header-box__nav__list', { delay: 200, origin: "top" });
 ScrollReveal().reveal('.txt-center', { delay: 200, origin: "bottom" });
 ScrollReveal().reveal('.scrool', { delay: 200, origin: "top" });
 
+ScrollReveal().reveal('.main-box__image', { delay: 200, origin: "left" });
+ScrollReveal().reveal('.main-box__image__txt', { delay: 200, origin: "right" });
 ScrollReveal().reveal('.main__skill__list__image-html', { delay: 200, origin: "left" });
 
 ScrollReveal().reveal('.main__skill__list__image-css', { delay: 200, origin: "right" });
